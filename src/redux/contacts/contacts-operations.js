1b798cfd3b33d920c5e129c692167378d26d56b1
Anastasia Solovei <av.ignatyuk@gmail.com>
2021-09-16 17:37:18 +0300

    creates operations with using actions request, succsess, error

---
+++ src/redux/contacts/contacts-operations.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+import {
+  fetchContactsRequest,
+  fetchContactsSuccess,
+  fetchContactsError,
+  addContactRequest,
+  addContactSuccess,
+  addContactError,
+  deleteContactRequest,
+  deleteContactSuccess,
+  deleteContactError,
+} from "./contacts-actions";
+
+axios.defaults.baseURL = "http://localhost:4040";
+
+export const fetchContacts = () => (dispatch) => {
+  dispatch(fetchContactsRequest());
+
+  axios
+    .get("/contacts")
+    .then(({ data }) => dispatch(fetchContactsSuccess(data)))
+    .catch((error) => dispatch(fetchContactsError(error)));
+};
+
+export const addContact = (contact) => (dispatch) => {
+  dispatch(addContactRequest());
+
+  axios
+    .post("/contacts", contact)
+    .then(({ data }) => dispatch(addContactSuccess(data)))
+    .catch((error) => dispatch(addContactError(error)));
+};
+
+export const deleteContact = (contactId) => (dispatch) => {
+  dispatch(deleteContactRequest());
+
+  axios
+    .delete(`/contacts/${contactId}`)
+    .then(() => dispatch(deleteContactSuccess(contactId)))
+    .catch((error) => dispatch(deleteContactError(error)));
+};
