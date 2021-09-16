1504d0735f81b9279ea580f00721bf2a6f116b4c
Anastasia Solovei <av.ignatyuk@gmail.com>
2021-09-16 18:06:14 +0300

    refactoring code of operations and reducer with createAsyncThunk

--- src/redux/contacts/contacts-operations.js
+++ src/redux/contacts/contacts-operations.js
@@ -1,41 +1,28 @@
-import axios from "axios";
-import {
-  fetchContactsRequest,
-  fetchContactsSuccess,
-  fetchContactsError,
-  addContactRequest,
-  addContactSuccess,
-  addContactError,
-  deleteContactRequest,
-  deleteContactSuccess,
-  deleteContactError,
-} from "./contacts-actions";
+import { createAsyncThunk } from "@reduxjs/toolkit";
 
+import axios from "axios";
 axios.defaults.baseURL = "http://localhost:4040";
 
-export const fetchContacts = () => (dispatch) => {
-  dispatch(fetchContactsRequest());
+export const fetchContacts = createAsyncThunk(
+  "contactList/fetchContacts",
+  async () => {
+    const contacts = await axios.get("/contacts");
+    return contacts.data;
+  }
+);
 
-  axios
-    .get("/contacts")
-    .then(({ data }) => dispatch(fetchContactsSuccess(data)))
-    .catch((error) => dispatch(fetchContactsError(error)));
-};
+export const addContact = createAsyncThunk(
+  "contactList/addContact",
+  async (item) => {
+    const contact = await axios.post("/contacts", item);
+    return contact.data;
+  }
+);
 
-export const addContact = (contact) => (dispatch) => {
-  dispatch(addContactRequest());
-
-  axios
-    .post("/contacts", contact)
-    .then(({ data }) => dispatch(addContactSuccess(data)))
-    .catch((error) => dispatch(addContactError(error)));
-};
-
-export const deleteContact = (contactId) => (dispatch) => {
-  dispatch(deleteContactRequest());
-
-  axios
-    .delete(`/contacts/${contactId}`)
-    .then(() => dispatch(deleteContactSuccess(contactId)))
-    .catch((error) => dispatch(deleteContactError(error)));
-};
+export const deleteContact = createAsyncThunk(
+  "contactList/deleteContact",
+  async (contactId) => {
+    await axios.delete(`/contacts/${contactId}`);
+    return contactId;
+  }
+);
